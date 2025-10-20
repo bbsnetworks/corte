a3f490acc4292635d2b2b9a1d87a0afec86b1761
bbsnetworks <bbsnetworks0@gmail.com>
2025-10-20 11:19:13 -0600

    correcciones integrity

--- total/index.php
+++ total/index.php
@@ -5,8 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Corte BBS</title>
-  <link href="../css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link href="../css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
   <link rel="stylesheet" href="../css/bootstrap-icons.min.css">
   <link rel="stylesheet" href="../css/generales.css">
   <link rel="stylesheet" href="../css/total.css">
@@ -262,9 +261,7 @@ if (!isset($_SESSION['username'])) {
     </div>
   </div>
   <!-- Modal -->
-  <script src="../js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-    crossorigin="anonymous"></script>
+  <script src="../js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
   <script src="../js/jquery-3.7.1.min.js"></script>
   <script src="../js/moment.min.js"></script>
   <script src="../js/sweetalert2@11.js"></script>
